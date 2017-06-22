fd14d2d754993589b2ddf2b75e8d09e9ecafb452
Venilton FalvoJr <falvojr@gmail.com>
2017-06-22 15:29:51 -0300

    Fix dependencies

---
+++ src/main/java/br/usp/scc5949/Main.java
@@ -0,0 +1,9 @@
+package br.usp.scc5949;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ src/test/java/br/usp/scc5949/MainTest.java
@@ -0,0 +1,10 @@
+package br.usp.scc5949;
+
+import org.junit.Test;
+
+public class MainTest {
+	
+    @Test public void testMain() {
+        //TODO Unit tests
+    }
+}
