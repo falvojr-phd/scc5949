1254a7de24b581c72460b4b0a80779a398105615
Venilton FalvoJr <falvojr@gmail.com>
2017-07-03 17:47:54 -0300

    Full implementation v0

--- src/main/java/br/usp/scc5949/Main.java
+++ src/main/java/br/usp/scc5949/Main.java
@@ -3,61 +3,109 @@ package br.usp.scc5949;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-
-import org.apache.commons.lang.ArrayUtils;
+import java.util.Map;
 
 import com.fasterxml.jackson.databind.MappingIterator;
 import com.fasterxml.jackson.dataformat.csv.CsvMapper;
 import com.fasterxml.jackson.dataformat.csv.CsvParser;
+import com.google.common.collect.Lists;
 
 import br.usp.scc5949.model.Node;
 
 public class Main {
+	
+	private static final ClassLoader CLASS_LOADER = Thread.currentThread().getContextClassLoader();
 
 	public static void main(String[] args) {
 		try {
-			final ClassLoader loader = Thread.currentThread().getContextClassLoader();
-			String path = loader.getResource("bc.csv").getFile();
-			Main.readCsvResource(path);
-			
+			Main.solveBc();
+			Main.solveNhl();
 		} catch (IOException e) {
-			e.printStackTrace();
+			System.err.println(e);
 		}
 	}
 
-	public static List<Node> readCsvResource(final String path) throws IOException {
-		final File csvFile = new File(path);
+	public static void solveBc() throws IOException {
+		final Map<Integer, List<Integer>> bcInfluences = new HashMap<>();
+		bcInfluences.put(0, Lists.newArrayList(0));
+		bcInfluences.put(1, Lists.newArrayList(1));
+		bcInfluences.put(2, Lists.newArrayList(2));
+		bcInfluences.put(3, Lists.newArrayList(1, 2, 3));
+		bcInfluences.put(4, Lists.newArrayList(0, 3, 4));
+		bcInfluences.put(5, Lists.newArrayList(3, 5));
+		bcInfluences.put(6, Lists.newArrayList(3, 6));
+		bcInfluences.put(7, Lists.newArrayList(3, 7));
+		bcInfluences.put(8, Lists.newArrayList(4, 8));
+		bcInfluences.put(9, Lists.newArrayList(4, 9));
+		bcInfluences.put(10, Lists.newArrayList(5, 10));
+		bcInfluences.put(11, Lists.newArrayList(6, 11));
+		bcInfluences.put(12, Lists.newArrayList(3, 10, 12));
+		bcInfluences.put(13, Lists.newArrayList(3, 10, 13));
+		bcInfluences.put(14, Lists.newArrayList(10, 14));
+		bcInfluences.put(15, Lists.newArrayList(4, 14, 15));
+
+		final String path = CLASS_LOADER.getResource("bc.csv").getFile();
+		Main.whritePossibilitiesTables(path, bcInfluences);
+	}
+
+	public static void solveNhl() throws IOException {
+		final Map<Integer, List<Integer>> nhlInfluences = new HashMap<>();
+		nhlInfluences.put(0, Lists.newArrayList(0));
+		nhlInfluences.put(1, Lists.newArrayList(0, 1));
+		nhlInfluences.put(2, Lists.newArrayList(0, 3, 4, 2));
+		nhlInfluences.put(3, Lists.newArrayList(0, 3));
+		nhlInfluences.put(4, Lists.newArrayList(0, 4));
+		nhlInfluences.put(5, Lists.newArrayList(5));
+		nhlInfluences.put(6, Lists.newArrayList(6));
+		nhlInfluences.put(7, Lists.newArrayList(7));
+		nhlInfluences.put(8, Lists.newArrayList(0, 6, 8));
+		nhlInfluences.put(9, Lists.newArrayList(6, 9));
+		nhlInfluences.put(10, Lists.newArrayList(6, 10));
+		nhlInfluences.put(11, Lists.newArrayList(2, 3, 4, 6, 7, 12, 11));
+		nhlInfluences.put(12, Lists.newArrayList(8, 9, 10, 12));
+		nhlInfluences.put(13, Lists.newArrayList(0, 2, 3, 4, 11, 13));
+
+		final String path = CLASS_LOADER.getResource("nhl.csv").getFile();
+		Main.whritePossibilitiesTables(path, nhlInfluences);
+	}
+
+	public static List<Node> whritePossibilitiesTables(final String path, Map<Integer, List<Integer>> influences)
+			throws IOException {
 		final CsvMapper mapper = new CsvMapper();
 		mapper.enable(CsvParser.Feature.WRAP_AS_ARRAY);
-		final MappingIterator<String[]> it = mapper.readerFor(String[].class).readValues(csvFile);
+		final MappingIterator<String[]> it = mapper.readerFor(String[].class).readValues(new File(path));
+
 		final List<Node> nodes = new ArrayList<>();
+
 		if (it.hasNext()) {
 			final String[] headers = it.next();
-			for (String header : headers) {
-				nodes.add(new Node(header));
-			}
-			while (it.hasNext()) {
-				final String[] values = it.next();
-				for (int i = 0; i < values.length; i++) {
-					String value = values[i];
-					nodes.get(i).getValues().add(value);
-				}
-			}
-			for (Node node : nodes) {
-				node.evalMarginalProbabilities();
-				node.print();
+			for (int i = 0; i < headers.length; i++) {
+				final String name = headers[i];
+				nodes.add(new Node(name, influences.get(i)));
 			}
 		}
-		return nodes;
-	}
-	
-
-	public static void printCsvResource(final MappingIterator<String[]> it) {
+		
 		while (it.hasNext()) {
-			final String[] row = it.next();
-			System.out.println(ArrayUtils.toString(row));
+			final String[] values = it.next();
+			for (int i = 0; i < values.length; i++) {
+				final Node node = nodes.get(i);
+				
+				String preparedValue = "";
+				for (Integer influence : node.getInfluences()) {
+					preparedValue += String.format("%-13s", values[influence]);
+				}
+				node.getValues().add(preparedValue);
+			}
 		}
+
+		for (Node node : nodes) {
+			node.evalProbabilities();
+			node.print();
+		}
+		
+		return nodes;
 	}
 
 }
--- src/main/java/br/usp/scc5949/model/Node.java
+++ src/main/java/br/usp/scc5949/model/Node.java
@@ -1,31 +1,47 @@
 package br.usp.scc5949.model;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
 
-import br.usp.scc5949.util.MapUtil;
+import com.google.common.collect.Multiset.Entry;
+import com.google.common.collect.TreeMultiset;
+
 import dnl.utils.text.table.TextTable;
 
 public class Node {
-	
-	private String id;
+
+	private String name;
+	private List<Integer> influences;
 	private List<String> values;
 	private Map<String, Double> probabilities;
-	private List<Node> parents;
 
-	public Node(String id) {
-		this.id = id;
+	public Node(String name, List<Integer> influences) {
+		this.name = name;
+		this.influences = influences;
 	}
 
-	public String getId() {
-		return id;
+	public String getName() {
+		return name;
 	}
 
-	public void setId(String id) {
-		this.id = id;
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Integer> getInfluences() {
+		if (influences == null) {
+			influences = new ArrayList<>();
+		}
+		return influences;
+	}
+
+	public void setInfluences(List<Integer> influences) {
+		this.influences = influences;
 	}
 
 	public List<String> getValues() {
@@ -40,9 +56,6 @@ public class Node {
 	}
 
 	public Map<String, Double> getProbabilities() {
-		if (probabilities == null) {
-			probabilities = new HashMap<>();
-		}
 		return probabilities;
 	}
 
@@ -50,34 +63,62 @@ public class Node {
 		this.probabilities = probabilities;
 	}
 
-	public List<Node> getParents() {
-		if (parents == null) {
-			parents = new ArrayList<>();
+	public void evalProbabilities() {
+		if (this.influences.size() == 1) {
+			final Double sum = (double) this.values.size();
+			final Map<String, Double> mappedValues = this.values.stream()
+					.collect(Collectors.groupingBy(e -> e, Collectors.counting())).entrySet().stream()
+					.collect(Collectors.toMap(Map.Entry::getKey, e -> evalAverage(e.getValue(), sum)));
+			this.probabilities = new TreeMap<>(mappedValues);
+
+		} else {
+			this.probabilities = new TreeMap<>();
+
+			String groupKey = "";
+			final Map<String, Long> auxProbabilities = new TreeMap<>();
+
+			final TreeMultiset<String> ordenedValues = TreeMultiset.create(this.values);
+			final Set<Entry<String>> entrySet = ordenedValues.entrySet();
+			for (Entry<String> entry : entrySet) {
+				final String[] values = entry.getElement().split("\\s+");
+				final String key = Arrays.toString(Arrays.copyOf(values, values.length - 1));
+				if (!groupKey.equals(key)) {
+					groupKey = key;
+					if (!groupKey.isEmpty()) {
+						final Double sum = auxProbabilities.values().stream().mapToDouble(Number::doubleValue).sum();
+						final Map<String, Double> mappedValues = auxProbabilities.entrySet().stream()
+								.collect(Collectors.toMap(Map.Entry::getKey, e -> evalAverage(e.getValue(), sum)));
+						this.probabilities.putAll(mappedValues);
+						auxProbabilities.clear();
+					}
+				}
+				auxProbabilities.put(entry.getElement(), Long.valueOf(entry.getCount()));
+			}
 		}
-		return parents;
-	}
-
-	public void setParents(List<Node> parents) {
-		this.parents = parents;
-	}
-
-	public void evalMarginalProbabilities() {
-		this.probabilities = this.values.stream()
-				.collect(Collectors.groupingBy(e -> e, Collectors.counting()))
-				.entrySet().stream()
-				.collect(Collectors.toMap(Map.Entry::getKey, e -> evalAverage(e.getValue())));
 	}
 
 	public void print() {
-		final String[] header = { this.id, "Probabilities" };
-		final Object[][] data = MapUtil.parseToMatrix(this.probabilities);
-		final TextTable textTable = new TextTable(header , data);
+		final String[] header = { this.name, "Probabilities" };
+		final Object[][] data = getMatrixProbabilities();
+		final TextTable textTable = new TextTable(header, data);
 		textTable.printTable();
 		System.out.println();
 	}
 
-	private double evalAverage(Long valueCount) {
-		final double valuesCount = (double) this.values.size();
-		return valueCount / valuesCount;
+	private double evalAverage(Long valueCount, Double sum) {
+		return valueCount / sum;
+	}
+
+	private Object[][] getMatrixProbabilities() {
+		Object[][] twoDarray = new Object[this.probabilities.size()][2];
+
+		Object[] keys = this.probabilities.keySet().toArray();
+		Object[] values = this.probabilities.values().toArray();
+
+		for (int row = 0; row < twoDarray.length; row++) {
+			twoDarray[row][0] = keys[row];
+			twoDarray[row][1] = values[row];
+		}
+		return twoDarray;
 	}
 }
